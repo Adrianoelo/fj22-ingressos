e04482258888cab2dbb39426ccc3ed315538b70d
Adriano Rodrigues <adriano_rodrigues_fsa@yahoo.com.br>
2019-01-29 16:19:02 -0200

    Incluindo Preços na sala, filme e sessão

--- src/main/java/br/com/caelum/ingresso/model/Sala.java
+++ src/main/java/br/com/caelum/ingresso/model/Sala.java
@@ -3,6 +3,9 @@ package br.com.caelum.ingresso.model;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -21,6 +24,9 @@ public class Sala {
 
     @OneToMany(fetch = FetchType.EAGER)
     private List<Lugar> lugares = new ArrayList<>();
+    
+    private BigDecimal preco;
+    
 
     /**
      * @deprecated hibernate only
@@ -29,11 +35,24 @@ public class Sala {
 
     }
 
-    public Sala(String nome) {
+    public Sala(String nome, BigDecimal preco) {
         this.nome = nome;
+        this.preco = preco;
     }
 
-    public Integer getId() {
+    public BigDecimal getPreco() {
+    	if (preco != null) {
+    		return preco.setScale(2, RoundingMode.HALF_UP);
+    	} else {
+    		return BigDecimal.ZERO;
+    	}
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getId() {
         return id;
     }
 
